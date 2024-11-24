453898c15efc707e953723b71127dac45409308c
Kejdi <kejdishameti@gmail.com>
2024-11-24 22:26:08 +0100

    add course model

---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Core.Pipeline;
+using Microsoft.Identity.Client;
+
+namespace StudentCourseManager.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
