3db64db96bb1f0a065dd98bd196b7bbe5d179302
Kejdi <kejdishameti@gmail.com>
2024-11-24 22:24:00 +0100

    add-student-model

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentCourseManager.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
