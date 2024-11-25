52eec76cd19314b2ddaa0e9fffd62b1e6075b987
Kejdi <kejdishameti@gmail.com>
2024-11-25 15:12:31 +0100

    Update Course model to include Credits

--- Models/Course.cs
+++ Models/Course.cs
@@ -8,6 +8,7 @@ namespace StudentCourseManager.Models
         public int ID { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
+        public int Credits { get; set; }
         public ICollection<Enrollment>? Enrollments { get; set; }
     }
 }
