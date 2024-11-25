1a45aae43243e51ce5faadbf4b458b5c51b0b0b1
Kejdi <kejdishameti@gmail.com>
2024-11-25 15:07:59 +0100

    Update Student model to include DateOfBirth

--- Models/Student.cs
+++ Models/Student.cs
@@ -3,8 +3,9 @@
     public class Student
     {
         public int ID { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public ICollection<Enrollment>? Enrollments { get; set; }
     }
 }
